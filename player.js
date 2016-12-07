bfd6e5cac7f58134508b09813e9911fa28a5d0d0
= <=>
2016-12-06 22:36:12 -0500

    class clean up

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-function Player() {
+var Player = function () {
 	this.player_name = "tommosfool";
 
 	this.x = 100;
@@ -18,94 +18,88 @@ function Player() {
 		"speed" : 3
 	};
 	this.path = [];
-
-	this.draw = function () {
-		colorMode(HSB, 360, 100, 100);
-		fill(this.color_h, this.color_s, this.color_b);
-		rect(this.x-this.w/2, this.y-this.h, this.w, this.h);
-		colorMode(RGB, 255);
-		if (this.flags['talk_to'] === true) {
-			fill(255);
-			ellipse(this.x-this.w/2, this.y-this.h, this.w, this.w);
+};
+Player.prototype.draw = function () {
+	colorMode(HSB, 360, 100, 100);
+	fill(this.color_h, this.color_s, this.color_b);
+	rect(this.x-this.w/2, this.y-this.h, this.w, this.h);
+	colorMode(RGB, 255);
+	if (this.flags['talk_to'] === true) {
+		fill(255);
+		ellipse(this.x-this.w/2, this.y-this.h, this.w, this.w);
+	}
+};
+Player.prototype.keyPressed = function (key) {
+	if (key == 'T' && this.flags['talk_to']) {
+		this.flags['talk_to_npc'].talk_to();
+	}
+};
+Player.prototype.update = function (npcs) {
+	var mx = 0;
+	var my = 0;
+	if (keyIsDown(87)) {//w
+		my -= 1;
+	}
+	if (keyIsDown(83)){//s
+		my += 1;
+	}
+	if (keyIsDown(65)){//a
+		mx -= 1;
+	}
+	if (keyIsDown(68)){//d
+		mx += 1;
+	}
+	this.move(mx, my);
+	
+	this.flags['talk_to'] = false;
+	this.flags['talk_to_npc'] = null;
+	var unmoved = false;
+	for (var i = npcs.length - 1; i >= 0; i--) {
+		var x = npcs[i].x;
+		var y = npcs[i].y;
+		if (this.colide_circle(x, y, npcs[i].w/2)) { // move to begining 
+			this.unmove();
+			unmoved = true;
 		}
-	};
-
-	this.keyPressed = function (key) {
-		if (key == 'T' && this.flags['talk_to']) {
-			this.flags['talk_to_npc'].talk_to();
-		}
-	};
-
-	this.update = function (npcs) {
-		var mx = 0;
-		var my = 0;
-		if (keyIsDown(87)) {//w
-			my -= 1;
-		}
-		if (keyIsDown(83)){//s
-			my += 1;
-		}
-		if (keyIsDown(65)){//a
-			mx -= 1;
-		}
-		if (keyIsDown(68)){//d
-			mx += 1;
-		}
-		this.move(mx, my);
-		
-		this.flags['talk_to'] = false;
-		this.flags['talk_to_npc'] = null;
-		var unmoved = false;
-		for (var i = npcs.length - 1; i >= 0; i--) {
-			var x = npcs[i].x;
-			var y = npcs[i].y;
-			if (this.colide_circle(x, y, npcs[i].w/2)) { // move to begining 
-				this.unmove();
-				unmoved = true;
-			}
-			if (this.colide_circle(x, y, npcs[i].w/2+20)) {
-				if (this.flags['talk_to'] && (this.flags['talk_to_npc'] != npcs[i])) {
-					var old = this.flags['talk_to_npc'];
-					var cur = npcs[i];
-					var old_dist = dist(this.x, this.y, old.x, old.y);
-					var cur_dist = dist(this.x, this.y, cur.x, cur.y);
-					if (cur_dist < old_dist) {
-						this.flags['talk_to_npc'] = cur;
-					}
-				}else {
-					this.flags['talk_to'] = true;
-					this.flags['talk_to_npc'] = npcs[i];
+		if (this.colide_circle(x, y, npcs[i].w/2+20)) {
+			if (this.flags['talk_to'] && (this.flags['talk_to_npc'] != npcs[i])) {
+				var old = this.flags['talk_to_npc'];
+				var cur = npcs[i];
+				var old_dist = dist(this.x, this.y, old.x, old.y);
+				var cur_dist = dist(this.x, this.y, cur.x, cur.y);
+				if (cur_dist < old_dist) {
+					this.flags['talk_to_npc'] = cur;
 				}
+			}else {
+				this.flags['talk_to'] = true;
+				this.flags['talk_to_npc'] = npcs[i];
 			}
 		}
-		if (unmoved === false && terrain.colide(this.x, this.y, this.w/2) && !this.flags['noclip']) {
-			console.log('collllllllide');
-			this.unmove();
-			unmoved = true;
-		}
-		if (unmoved === false && windows.open_window) {
-			this.unmove();
-			unmoved = true;
-		}
-	};
-
-	this.move = function (x, y, spd) {
-		spd = spd || this.attribs['speed'] + (this.flags['spd_buf'] || 0);
-		this.x += x*spd;
-		this.y += y*spd;
-		var maxPath = 10;
-		for (var i = 1; i < maxPath; i++) {
-			this.path[i-1] = this.path[i];
-		}
-		this.path.push([x, y, spd]);
-	};
-
-	this.unmove = function () {
-		var path = this.path[this.path.length-1];
-		this.move(-path[0], -path[1], path[2]);
-	};
-
-	this.colide_circle = function (x, y, r) {
-		return dist(this.x, this.y, x, y) < (r + this.w/2);
-	};
-}
+	}
+	if (unmoved === false && terrain.colide(this.x, this.y, this.w/2) && !this.flags['noclip']) {
+		console.log('collllllllide');
+		this.unmove();
+		unmoved = true;
+	}
+	if (unmoved === false && windows.open_window) {
+		this.unmove();
+		unmoved = true;
+	}
+};
+Player.prototype.move = function (x, y, spd) {
+	spd = spd || this.attribs['speed'] + (this.flags['spd_buf'] || 0);
+	this.x += x*spd;
+	this.y += y*spd;
+	var maxPath = 10;
+	for (var i = 1; i < maxPath; i++) {
+		this.path[i-1] = this.path[i];
+	}
+	this.path.push([x, y, spd]);
+};
+Player.prototype.unmove = function () {
+	var path = this.path[this.path.length-1];
+	this.move(-path[0], -path[1], path[2]);
+};
+Player.prototype.colide_circle = function (x, y, r) {
+	return dist(this.x, this.y, x, y) < (r + this.w/2);
+};
